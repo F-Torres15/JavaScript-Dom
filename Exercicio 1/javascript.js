5092050a2e9db02fa21c1a8fd9b399af7a32fca8
F-Torres15 <fx109090@gmail.com>
2024-10-23 20:02:55 -0300

    em Porcesso

--- Exercicio 1/javascript.js
+++ Exercicio 1/javascript.js
@@ -1,19 +1,24 @@
-clickfundo = 1
 
 
 function corFundo(){
-    if (click ==1){
+let fund = document.querySelector ("body")
+let titulo = document.querySelector('h1')
+let p = document.querySelector('p')
+    if (fund.style.backgroundColor=="white"){
 
-    
-    let fundo = document.querySelector('body')
-    fundo.style.backgroundColor = 'black'
-    let titulo = document.querySelector('h1')
-    titulo.style.color ='white' 
-    let p = document.querySelector('p')
+
+    fund.style.backgroundColor = "black"
+    titulo.style.color ="white"
     p.style.color='white'   
-    click =0
+
+
     }
     else {
+        fund.style.backgroundColor= "white"
+        titulo.style.color="black"
+        p.style.color="black"
+
+
 
 
     }
@@ -21,14 +26,32 @@ function corFundo(){
 
 function corTexto(){
 let cor = document.querySelector('p')
-cor.style.cor = 'blue'
+if(cor.style.color == "black"){
+cor.style.cor = "blue"
+} else{
+    cor.style.color = "black"
+}
 }
 
 function aumentarFont(){
-    let fonte = document.querySelectorAll('p')[1]
-    if(fonte){
-        fonte.style.style.fontSize ='50px'
+    let p = document.querySelector('p')[1]
+    if(p.style.fontSize ="10px"){
+        p.style.fontSize ="30px"
+    } else {
+        p.style.fontSize="10px"
     }
     }
+click ==0
+function alterarText(){
+    let p = document.querySelector('p')
+    if(  p = document.querySelector('p')){
+    p.innerText= "Até a última terça-feira, pouco mais de um terço do gramado já havia sido recolocado. O trabalho, basicamente, consiste em: máquinas deixam os rolos de grama posicionados para que os funcionários os abram e fixem ao solo. Depois, outro aparelho 'ajeita' os pedaços para não sobrar nenhum espaço entre eles."
+    }
+    else {
+        click ==1
+        p=document.querySelector('p')
+
+    }
+}
 
     
